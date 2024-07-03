7d0f74b0a7900538f74e8eb04761c9a8db6d9763
Simão Antunes <simaopfantunes15@duck.com>
2024-07-03 11:33:35 +0100

    Add files via upload

---
+++ Guioes/Guiao7/src/ex1/ContactList.java
@@ -0,0 +1,16 @@
+package ex1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+class ContactList extends ArrayList<Contact> {
+
+    // @TODO
+    public void serialize(DataOutputStream out) throws IOException { }
+
+    // @TODO
+    public static ContactList deserialize(DataInputStream in) throws IOException { }
+
+}
---
+++ Guioes/Guiao7/src/ex4/ContactList.java
@@ -0,0 +1,26 @@
+package ex4;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+class ContactList extends ArrayList<Contact> {
+
+    // @TODO
+    public void serialize(DataOutputStream out) throws IOException {
+        out.writeInt(size());
+        for (Contact c : this)
+            c.serialize(out);
+    }
+
+    // @TODO
+    public static ContactList deserialize(DataInputStream in) throws IOException {
+        int size = in.readInt();
+        ContactList cl = new ContactList();
+        for (int i = 0; i < size; i++)
+            cl.add(Contact.deserialize(in));
+        return cl;
+    }
+
+}
